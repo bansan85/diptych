113b7258554ca6b22d2be8d772cf91742a697593
LE GARREC Vincent <github@le-garrec.fr>
2020-10-27 07:30:47 +0100

    Add Mock for SeparatePage

---
+++ fsext.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import os
+import shutil
+
+
+def del_pattern(folder: str, pattern: str) -> None:
+    for path in Path(folder).glob(pattern):
+        path.unlink()
+
+
+def is_file_exists(filename: str) -> bool:
+    return os.path.exists(filename)
+
+
+def copy_file(source: str, destination: str) -> None:
+    shutil.copyfile(source, destination)
+
+
+def get_absolute_from_current_path(module_filename: str, filename: str) -> str:
+    return os.path.join(
+        os.path.dirname(os.path.abspath(module_filename)), filename
+    )
+
+
+def extract_path(full_path: str) -> str:
+    return os.path.dirname(os.path.abspath(full_path))
+
+
+def delete_file(filename: str) -> None:
+    os.remove(filename)
