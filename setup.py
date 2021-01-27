7a8d4ab4736af1e79cd0b97e0498630efb5d5581
LE GARREC Vincent <github@le-garrec.fr>
2021-01-27 22:17:47 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("LICENSE") as f:
+    license_text = f.read()
+
+setup(
+    name="diptych",
+    version="0.0.1",
+    description=(
+        "Detect multiple pages in image scanned, split them and apply OCR."
+    ),
+    long_description=readme,
+    author="Vincent LE GARREC",
+    author_email="github@le-garrec.fr",
+    url="https://github.com/bansan85/diptych",
+    license=license_text,
+    packages=find_packages(exclude=("tests", "docs")),
+)
